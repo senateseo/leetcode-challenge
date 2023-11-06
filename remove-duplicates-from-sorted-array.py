6eff1feee255f15834e871abe0a0cd4b96fbcef5
senateseo <swdanielseo@gmail.com>
2023-11-05 22:31:39 -0800

    Day3.

---
+++ remove-duplicates-from-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        k=1
+        for i in range(len(nums)-1):
+            if(nums[i]!=nums[i+1]):
+                nums[k]=nums[i+1]
+                k+=1
+        return k
+        
+        # Keypoint is overwriting the elements was passed in array.
