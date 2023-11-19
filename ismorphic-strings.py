b7f62977aa1761d18fafe0abb4cdf3106231cafe
senateseo <swdanielseo@gmail.com>
2023-11-18 22:43:01 -0800

    Day 11

---
+++ ismorphic-strings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_hsm={}
+        t_hsm={}
+        
+        for i in range(len(s)):
+            if s[i] in s_hsm:
+                if s_hsm[s[i]] != t[i]:
+                    return False
+            else:
+                s_hsm[s[i]]=t[i]
+
+            if t[i] in t_hsm:
+                if t_hsm[t[i]] != s[i]:
+                    return False
+            else:
+                t_hsm[t[i]]=s[i]
+        return True
+
+        
+        # badc
+        # baba
+
+
+        # When Key is not found in Hashmap
+        # - put key as s[i] and value as t[i]
